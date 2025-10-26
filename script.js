271076fbe838ecd40ff29fe870a8d16b73bb5f9d
sayyadshahi8252 <167566299+sayyadshahi8252@users.noreply.github.com>
2025-10-26 12:05:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function incrementBtn(){
+	const targetingcounter=document.getElementById("counter");
+	let initialvalue=Number(targetingcounter.textContent)
+	alert(initialvalue)
+	targetingcounter.textContent=initialvalue+1;
+}
